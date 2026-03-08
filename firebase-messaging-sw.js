adbfd8a9ec9dda9acaf183d50cfc3b3c81af79c4
modoption <m93.alshehri@gmail.com>
2026-03-08 20:35:34 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,13 +1,23 @@
-{
-  "type": "service_account",
-  "project_id": "mod-options-lmql3k",
-  "private_key_id": "7c3116d812d653edd9224736c41d0c621d0faa18",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDQt5L/2sITqsDt\nMxbehW9xiKknoczGayx39hpacIODHuCl84ZfQUCUhOSgKu5Zz8Ra9RZqRcC7fmkl\n3aHBXbaobp485nJV4LLq5yAnZg0u1lsRIUXTnHPwNOTBst0R6ijcxPeVgXPGa2FX\nBEhAAtNXDc61PF/hpnIJCZtr0fgC0dtv9oi1gx/Qg+vhFehSVSLhAyb621IRmPh7\nf7U0JEgr32hJxtwNX/0R6JYmWjydajFQ5bj1dbsjZlaj29kWYHmZsRjvMRzzC6kE\nwthr4MOn9SWVm7BcUa5Y1splIEpw2fm7ZbWWMZ+5ek387FNympYnh6iacs1z1cV4\nHsGLWHV/AgMBAAECggEABsbvLv+bbagNu6X9CBytEbKuS2sliwvP1dVX5bfhjVBj\nWwAdTNqBjlECW3EJbEEizxFGOQ0qxOvF3kTkC7NPSwjMeo3BCKjTJm16zcyygOLU\n8ilyavyZhyMwilmUdBB2YzTCFUiuybKS5pBtCCcPVbRNEfVz54/QBBsbwNSAiRbY\nTuqww1P88TcphzaUeYiPaqy1hrnn14mqZemrp2EZj8Zvt+kBdf2Gp8Y6C2qDGXuQ\njYNvDI5GdMVytdKzz8qjJmagclIEONE5Wc9eD6i9j38RiI+QdijUiN5Ok2KHpvHX\nY6OdrzI6zwb12U8zMwexGR/+LfTyQhkRe/UfbjPogQKBgQD18njGbT/NoNC7LBEs\ns9Z4FkOhGAzIpFIvNbiMKwYm1X972uHn8jaxU4jvR6xmTYgvNkG5vFYr1bQKyyjZ\nQ8+tcGIpuMta3n/9xIduJT6mQ2RQNR36jqMZCJe+wJ1e9RM93Jap1PKyI6Ld6DfK\nWzfNAMlWAZ5UvsyNsSV4ssoytQKBgQDZP4lYp+6Yx0Qu+kx4PK+itB3QilVyJ81K\nSSNc4RZRWjl8/jSrFYI9oeBlOoWYPMugxXgjy7sbcsK4NHBliZv+FOJSOfNoPE+9\nK+9mixu2YwvlyEyBJRsC0sqpRhAHN0QMDZrqsduPHkpXBp/IV4TvjKCSXMjlkvOd\nMAspphvj4wKBgQDRlSE0Pg67xIl6po+wH0Poq1hQI7VpGRXrowSzdUpVt2NegOQ3\nCmFu9Ldg6BvSRcmCKeMSSprH2eEFvZ/+nNmRe4467X1k3uwErP+XbeeYOR93dfwo\n8nmrsHXXGD5Cl+cgtSQybwOtp852+RbpupevezcWsmRg5QJc0BMVao4BbQKBgQDB\nZOs6jfw76x/Khi0u2C8uMxNbgtn/zmjVGXiuhP2eHCiTGaT/LB2OCh29X7Rvyqcn\nDsMdco61J5sp6hyodzDzpdm3gzko8I+3gnYSngFkgpWjz4Qzbzm1cLrSp7k7AzrD\nVBU1mnEu3bMl95KkAS0pJtIGmSyKGiBeLNezEtk/KwKBgB6/6RqLW5W2LBIRt2Hx\nwNGCMEydl7JC9eQ+BAsMgxWNF1aqUmr5Pzhnp0/rnOrsukN/PIjwAIF5wuA1JVXW\nnPrQ9IhHgB6SO+8w/ErBZ4DGjSPiM7qio2M5YtdMmmMXruOScQ/zcOMjSpbOu/yR\n1x9cgK5907zo6V02am58wTlF\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-fbsvc@mod-options-lmql3k.iam.gserviceaccount.com",
-  "client_id": "112910247848370452070",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40mod-options-lmql3k.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-}
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDXY3ZTUpPAi2iL3Y_XvQReqneomTbYbd0",
+  authDomain: "mod-options-lmql3k.firebaseapp.com",
+  projectId: "mod-options-lmql3k",
+  storageBucket: "mod-options-lmql3k.firebasestorage.app",
+  messagingSenderId: "764195037121",
+  appId: "1:764195037121:web:2cae80f08af37921f3d940"
+});
+
+var messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  var title = payload.notification.title;
+  var body = payload.notification.body;
+  self.registration.showNotification(title, {
+    body: body,
+    icon: '/mod-app/icon-192.png',
+    vibrate: [200, 100, 200]
+  });
+});
